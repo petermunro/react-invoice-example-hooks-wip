563499234641e651c07e9907e5df7bb733fe2f0d
Peter Munro <peter@petermunro.org>
2019-07-14 10:58:16 -0700

    Add call to use LineItem.js temporarily from App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import LineItem from "./LineItem";
 
 function App() {
+  let initialLineItemData = {
+    quantity: 3,
+    desciption: "Widget",
+    unitPrice: 8
+  };
+
+  let [lineItemData, setLineItemData] = useState(initialLineItemData);
+
+  function handleChange(id, newLineItemData) {
+    setLineItemData(newLineItemData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineItem
+        id={1}
+        lineItemData={lineItemData}
+        onChange={handleChange}
+        onDelete={() =>
+          setLineItemData({
+            quantity: 0,
+            desciption: "",
+            unitPrice: 0
+          })
+        }
+      />
     </div>
   );
 }
